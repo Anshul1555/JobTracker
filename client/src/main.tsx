0c9007da5a70c55df81f9b0dd7f9a7c5dd737ee2
Anshul Sharma <AnshulSharma9366@gmail.com>
2025-05-20 18:10:20 -0600

    Adding folder structure

---
+++ client/src/main.tsx
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/profiles/:profileId',
+        element: <Profile />
+      }, {
+        path: '/me',
+        element: <Profile />
+      }
+    ]
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
